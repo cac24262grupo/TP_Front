1e5f10b1d3c954fd92ad7166b4677d0c856ae91f
Sebastian A. Petrini <spetrini@gmail.com>
2024-07-04 14:06:18 -0300

    Agregado de prueba de back para render

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from .database import DbSession
+from .conf import settings
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return {"ping": "pong"}
+
+
+@app.get("/actores")
+def list_samples():
+
+    with DbSession(settings.DB_URL) as session:
+        session.execute("SELECT * FROM actores")
+        actores = session.fetchall()
+
+    return jsonify(actores)
