6bb6c113baea72b3f527e6029ff757612bc220b6
Sebastian A. Petrini <spetrini@gmail.com>
2024-07-06 20:37:08 -0300

    Prueba Listar actores 9

--- src/static/js/listar_actores.js
+++ src/static/js/listar_actores.js
@@ -1,29 +1,21 @@
-
-
-function addActorRow(id_actor,nombre) {
+function addActorRow(id_actor, nombre) {
   const tableBody = document.querySelector("#tabla-actores tbody");
   const row = document.createElement("tr");
   row.id = `actor-${id_actor}`;
 
-  // Crear un elemento form para envolver los elementos del formulario
-  const form = document.createElement("form");
-  form.id = `actoresform-${id_actor}`;
+  row.innerHTML = `
+                    <form id='actoresform-${id_actor}'>
+                        <td hidden scope="row">'${id_actor}'></td>
+                        <td hidden scope="row">${nombre}</td>
+                        <td><input type="text" name="nombreNew" value = '${nombre}'></td>
+                        <td>
+                           <button class="edit-btn" type="submit">✔</button>
+                           <button class="delete-btn">Eliminar</button>
+                        </td>
+                    </form>        
+    `;
 
-
- // Agregar contenido al formulario, incluyendo los campos y botones
-  form.innerHTML = `
-    <td hidden scope="row">${id_actor}</td>
-    <td hidden scope="row">${nombre}</td>
-    <td><input type="text" name="nombreNew" value='${nombre}'></td>
-    <td>
-      <button class="edit-btn" type="submit">✔</button>
-      <button type="button" class="delete-btn">Eliminar</button>
-    </td>
-  `;
-    // Añadir el formulario al row en lugar de establecer innerHTML directamente en el row
-    row.appendChild(form);
-
-   
+    const actoresForm = document.querySelector(`#actoresform-${id_actor}`);
   
     const deleteButton = row.querySelector(".delete-btn");
     deleteButton.addEventListener("click", async () => {
@@ -36,7 +28,7 @@ function addActorRow(id_actor,nombre) {
     tableBody.appendChild(row);
 
   const editButton = row.querySelector(".edit-btn");
-  
+  const { nombreNew } = actoresForm.getElementsByTagName("nombreNew");
   editButton.addEventListener("click", async () => {
     const response = await fetch(`/api/actores/${id_actor}`, {
         method: "PUT",
@@ -45,17 +37,13 @@ function addActorRow(id_actor,nombre) {
         },
         body: JSON.stringify({
           id_actor : id_actor,
-          nombre : nombre,
+          nombre : nombreNew,
         }),
     });
   });  
-
-
-
-
-// Añadir el row al cuerpo de la tabla
-tableBody.appendChild(row);
+  tableBody.appendChild(row);
 };
+
 function rmActorRow(id_actor) {
   const row = document.querySelector(`#actor-${id_actor}`);
   row.remove();
