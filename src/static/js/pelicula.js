0e2c570aa613621607367c025eb1a312d5854a07
Sebastian A. Petrini <spetrini@gmail.com>
2024-07-07 19:22:51 -0300

    Pruebas Pelicula

--- src/static/js/pelicula.js
+++ src/static/js/pelicula.js
@@ -1,14 +1,73 @@
+const peliculaForm = document.querySelector("#pelicula-form");
+
+peliculaForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+  
+    
+    const peliculaTitulo = actorForm["tituloPelicula"].value;
+
+
+    const peliculaSinopsis = actorForm["sinopsisPelicula"].value;
+    const peliculaUrl = actorForm["urlPelicula"].value;
+    const peliculaAno = actorForm["anoPelicula"].value;
+    const peliculaDuracion = actorForm["duracionPelicula"].value;
+    const peliculaGenero = actorForm["idGenero"].value;
+    const peliculaCategoria = actorForm["categoriaPelicula"].value;
+
+    if (peliculaTitulo.trim() === "") {
+        alert("Por favor, ingrese un titulo de pelicula.");
+        return;
+        }
+    if (peliculaSinopsis.trim() === "") {
+        alert("Por favor, ingrese una sinopsis de pelicula.");
+        return;
+        }
+    if (peliculaUrl.trim() === "") {
+        alert("Por favor, ingrese una url de pelicula.");
+        return;
+        }
+    if (peliculaAno.trim() === "") {
+        alert("Por favor, ingrese un año de pelicula.");
+        return;
+        }
+    if (peliculaCategoria.trim() === "") {
+        alert("Por favor, ingrese una categoria de pelicula.");
+        return;
+    }
+    if (peliculaGenero.trim() === "") {
+        alert("Por favor, ingrese un genero de pelicula.");
+        return;
+    }
+ 
+    const response = await fetch(`/api/peliculas`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        titulo: peliculaTitulo,
+        sinopsis: peliculaSinopsis,
+        url_pelicula: peliculaUrl,
+        ano_extreno: peliculaAno,
+        duracion: peliculaDuracion,
+        genero: peliculaGenero,
+        categoria: peliculaCategoria,
+      }),
+    });
+    if (!response.ok) {
+        alert("Hubo un problema al agregar la pelicula.");
+        return;
+      }
+    const data = await response.json(); 
+    peliculaForm.reset();
+  });
+
+
 
 function addGeneroOption(id_genero, genero) {
     const opcionNew = document.getElementById("idGenero");
-
     opcionNew.innerHTML += `<option value="${id_genero}">${genero}</option>`;
-    
-   /* const opt = document.createElement("option");
-    opt.value = id_genero;
-    opt.text = genero;
-    opcionNew.appendChild(opt);*/
-  };
+};
 
 window.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch("/api/generos");
@@ -19,7 +78,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         genero.genero
       );
     }
-  });
+});
 
 
   /*
