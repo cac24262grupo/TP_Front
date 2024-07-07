d6972cc590b17583afceafd3123cbb162e3e4344
Sebastian A. Petrini <spetrini@gmail.com>
2024-07-07 17:48:00 -0300

    Pruebas Pelicula

---
+++ src/static/js/pelicula.js
@@ -0,0 +1,67 @@
+const peliculaForm = document.querySelector("#pelicula-form");
+
+
+function addGeneroOption(id_genero, genero) {
+    const opcion = document.querySelector("#form-select-genero");
+    const opt = document.createElement("option");
+    //opt.value = `genero-${id_genero}`;
+    //opt.text = genero;
+    opt.innerHTML = `
+                    <option value="${id_genero}" text="${genero}" />       
+      `;
+    
+      
+    
+    
+     
+    opcion.appendChild(opt);
+  };
+
+window.addEventListener("DOMContentLoaded", async () => {
+    const response = await fetch("/api/generos");
+    const data = await response.json();
+    for (genero of data) {
+      addGeneroOption(
+        genero.id_genero,
+        genero.genero
+      );
+    }
+  });
+
+/*
+generoForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+  
+    
+    const generoName = generoForm["nombreGenero"].value;
+    
+    if (generoName.trim() === "") {
+        alert("Por favor, ingrese un nombre del genero.");
+        return;
+      }
+ 
+    const response = await fetch(`/api/generos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        genero: generoName,
+      }),
+    });
+    if (!response.ok) {
+        alert("Hubo un problema al agregar el genero.");
+        return;
+      }
+    const data = await response.json(); 
+    generoForm.reset();
+  });
+
+
+
+
+
+
+<option th:each="agente : ${agentes}" th:value="${agente.id}" th:text="${agente.nombre}" />
+
+*/
